4ae231fd4306d6766925375d3c076ae6d31d6035
Eric <54077752+Ericgacoki@users.noreply.github.com>
2020-05-24 15:01:14 +0300

    update 1.5

--- app/src/main/java/com/ericg/debtsmanager/MyDebts.kt
+++ app/src/main/java/com/ericg/debtsmanager/MyDebts.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MyDebts : AppCompatActivity() {
-
+val num: Int? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_debts)
